f8268cc18b48f2830f8963b80fe99c6af3fd3919
Long handsome <>
2020-06-25 20:35:49 +0700

    ui

---
+++ src/views/Home.js
@@ -0,0 +1,82 @@
+import React from "react";
+
+/** Material core*/
+import { makeStyles } from "@material-ui/core/styles";
+import Icon from "@material-ui/core/Icon";
+import MessageIcon from '@material-ui/icons/Message';
+import ContactPhoneIcon from '@material-ui/icons/ContactPhone';
+import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
+
+/** Component */
+import GridContainer from "../components/GridContainer.js";
+import CardIcon from "../components/CardIcon.js";
+import CardHeader from "../components/CardHeader.js";
+import Card from "../components/Card.js";
+import GridItem from "../components/GridItem.js";
+
+/** Assets */
+import styles from "../assets/styles/views/dashboardStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function Home() {
+    const classes = useStyles();
+    return (
+        <div>
+            <GridContainer >
+                <GridItem xs={12} sm={6} md={3}>
+                    <Card>
+                        <CardHeader color="warning" stats icon>
+                            <CardIcon color="warning">
+                                <Icon> <MessageIcon /></Icon>
+                            </CardIcon>
+                            <p className={classes.cardCategory}>Tin nhắn</p>
+                            <h3 className={classes.cardTitle}>
+                                49<small></small>
+                            </h3>
+                        </CardHeader>
+                    </Card>
+                </GridItem>
+                <GridItem xs={12} sm={6} md={3}>
+                    <Card>
+                        <CardHeader color="success" stats icon>
+                            <CardIcon color="success">
+                                <Icon> <ContactPhoneIcon /></Icon>
+                            </CardIcon>
+                            <p className={classes.cardCategory}>Danh bạ</p>
+                            <h3 className={classes.cardTitle}>
+                                49<small></small>
+                            </h3>
+                        </CardHeader>
+                    </Card>
+                </GridItem>
+                <GridItem xs={12} sm={6} md={3}>
+                    <Card>
+                        <CardHeader color="danger" stats icon>
+                            <CardIcon color="danger">
+                                <Icon> <PhotoLibraryIcon /></Icon>
+                            </CardIcon>
+                            <p className={classes.cardCategory}>Hình ảnh</p>
+                            <h3 className={classes.cardTitle}>
+                                49<small></small>
+                            </h3>
+                        </CardHeader>
+                    </Card>
+                </GridItem>
+                <GridItem xs={12} sm={6} md={3}>
+                    <Card>
+                        <CardHeader color="info" stats icon>
+                            <CardIcon color="info">
+                                <Icon> <MessageIcon /></Icon>
+                            </CardIcon>
+                            <p className={classes.cardCategory}>Số tin nhắn</p>
+                            <h3 className={classes.cardTitle}>
+                                49<small></small>
+                            </h3>
+                        </CardHeader>
+                    </Card>
+                </GridItem>
+            </GridContainer>
+        </div>
+    );
+}
